adc6e4e3efc01bd022ebda8424d7995d5d4a617f
Hyman <elahyman@gmail.com>
2016-08-05 01:36:13 +0800

    SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,14 @@
 #-*- coding:utf-8 -*-
 
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 #创建flask应用
 app = Flask(__name__)
 
 #读取应用配置文件(config.py)
 app.config.from_object('config')
+db = SQLAlchemy(app)
 
 #加载视图
-from app import views
+from app import views, models
--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
+
+import os
+basedir = os.path.abspath(os.path.dirname(__name__))
+
+# login configuration
 CSRF_ENABLED = True
 SECRET_KEY = "P\x88\xe5\xc1-\x01QD3\xeb\x81@"
 OPENID_PROVIDERS = [
@@ -9,3 +14,7 @@ OPENID_PROVIDERS = [
     { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
     { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }
 ]
+
+# database configuration
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
